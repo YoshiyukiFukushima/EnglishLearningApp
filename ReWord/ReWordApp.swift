97ac926230c09aa2ee349578c2e773c6929bb5b8
Yoshiyuki <yoshi5271091@gmail.com>
2025-12-07 14:20:09 +0900

    Initial Commit

---
+++ ReWord/ReWordApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReWordApp.swift
+//  ReWord
+//
+//  Created by 福島克侑 on 2025/12/07.
+//
+
+import SwiftUI
+
+@main
+struct ReWordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
